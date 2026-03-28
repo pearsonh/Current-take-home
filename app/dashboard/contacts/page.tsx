eb946b92e4d4456e5b90e96c2843b30488c03e82
pearsonoh <henryopearson@gmail.com>
2026-03-28 14:30:32 -0400

    fix error for await searchParamms

--- app/dashboard/contacts/page.tsx
+++ app/dashboard/contacts/page.tsx
@@ -7,6 +7,7 @@ export default async function Page(
         query?: string;
     }
 }) {
+    searchParams = await searchParams;
     const query = searchParams?.query || '';
 
     return (
--- app/dashboard/pays/page.tsx
+++ app/dashboard/pays/page.tsx
@@ -15,6 +15,7 @@ export default async function Page({
         page?: string;
     }
 }) {
+    searchParams = await searchParams;
     const query = searchParams?.query || '';
     const currentPage = Number(searchParams?.page) || 1;
 
